dd8361ad8221199c39e0e4eb0202e6de6d156ddc
vaishali023 <vaishalichaudhary011@gmail.com>
2022-05-21 20:51:34 -0400

    Creating the Bricks

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let  score = 0;
+const brickRowCount = 9;
+const brickColumnCount = 5;
 
 // Create ball props
 const ball = {
@@ -25,6 +28,26 @@ const paddle = {
     dx: 0
   };
   
+// Create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+  };
+  
+  // Create bricks
+  const bricks = [];
+  for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = [];
+    for (let j = 0; j < brickColumnCount; j++) {
+      const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+      const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+      bricks[i][j] = { x, y, ...brickInfo };
+    }
+  }
 
 // Draw ball on canvas
 function drawBall() {
